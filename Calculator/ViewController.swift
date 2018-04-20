893315ce7b3589b38556644a486f78e08c8711a1
Vu Hoang <iosgroup112018@gmail.com>
2018-04-20 10:32:01 +0700

    Hoang's commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,87 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numberOnScreen: Double = 0
+    var previousNumber: Double = 0
+    var mathOperation = false
+    var operation = 0
+    
+    @IBOutlet weak var showpheptinh: UILabel!
+    @IBOutlet weak var label: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        for i in 1...19{
+            view.viewWithTag(i)?.layer.borderWidth = 1.0
+            view.layer.borderColor = UIColor.red.cgColor
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func numbers(_ sender: UIButton) {
+        if mathOperation == true{
+                label.text = String(sender.tag - 1 )
+                numberOnScreen = Double(label.text!)!
+                mathOperation = false
+        }
+        else {
+                label.text = label.text!  + String(sender.tag - 1)
+                numberOnScreen = Double(label.text!)!
+        }
+    }
+    @IBAction func button(_ sender: UIButton) {
+        if label.text != "" && sender.tag !=  18 {
+            previousNumber = Double(label.text!)!
+            
+            if sender.tag == 14 {
+                showpheptinh.text = "/"
+            }
+            if sender.tag == 15 {
+                showpheptinh.text = "X"
+            }
+            if sender.tag == 16 {
+                showpheptinh.text = "-"
+            }
+            if sender.tag == 17 {
+                showpheptinh.text = "+"
+            }
+            operation = sender.tag
+            mathOperation = true;
+        }
+        else if sender.tag == 18 {
+            if operation == 14 {
+                label.text = String(previousNumber / numberOnScreen)
+            }
+            else if operation == 15 {
+                label.text = String(previousNumber * numberOnScreen)
+            }
+            else if operation == 16 {
+                label.text = String(previousNumber - numberOnScreen)
+            }
+            else if operation == 17 {
+                label.text = String(previousNumber + numberOnScreen)
+            }
+        }
 
+    }
+    @IBAction func buttonClear(_ sender: UIButton) {
+        label.text = ""
+        previousNumber = 0
+        numberOnScreen = 0
+        operation = 0
+    }
+    @IBAction func buttonChangeStatus(_ sender: UIButton) {
+        numberOnScreen = -1 * numberOnScreen
+        label.text = String(numberOnScreen)
+    }
+    @IBAction func buttonPercent(_ sender: UIButton) {
+        numberOnScreen = numberOnScreen * 1/100
+        label.text = String(numberOnScreen)
+    }
 
 }
 
